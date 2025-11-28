49b240073249dcb6a002aae57ebb3188662e4b4a
Ty <tystackhouse@gmail.com>
2025-11-28 09:36:36 -0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,14 +1,17 @@
-variable "tenancy_ocid" {}
-variable "user_ocid" {}
-variable "fingerprint" {}
-variable "private_key_path" {}
-variable "region" {}
-variable "compartment_ocid" {}
+# Configure the OCI Provider
+terraform {
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = "~> 5.0"
+    }
+  }
+}
 
 provider "oci" {
-  tenancy = var.tenancy_ocid
-  user    = var.user_ocid
-  fingerprint = var.fingerprint
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
   private_key_path = var.private_key_path
-  region      = var.region
+  region           = var.region
 }
